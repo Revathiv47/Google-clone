dca79cdef6060466d598697e823f6ea2effc6d97
revathi <revathi040798@gmail.com>
2022-02-10 17:20:11 +0530

    commit

--- google-clone/src/Search.js
+++ google-clone/src/Search.js
@@ -1,43 +1,51 @@
-import React , {useState} from 'react'
-import './Search.css';
-import SearchIcon from '@material-ui/icons/Search';
-import MicIcon from '@material-ui/icons/Mic';
-import {useHistory} from 'react-router-dom';
-import {Button} from '@material-ui/core';
-import {useStateValue} from './StateProvider';
-import {actionTypes} from './reducer';
+import React, { useState } from "react";
+import "./Search.css";
+import SearchIcon from "@material-ui/icons/Search";
+import MicIcon from "@material-ui/icons/Mic";
+import { useHistory } from "react-router-dom";
+import { Button } from "@material-ui/core";
+import { useStateValue } from "./StateProvider";
+import { actionTypes } from "./reducer";
 
 function Search() {
+  const [state, dispatch] = useStateValue();
 
-    const [state,dispatch] = useStateValue();
+  const [input, setInput] = useState("");
+  const history = useHistory();
 
-    const[input, setInput] = useState('')
-    const history = useHistory();
-    
-    const search = e => {
-        e.preventDefault();
+  const search = (e) => {
+    e.preventDefault();
 
-        history.push('/search')
+    history.push("/search");
 
-        dispatch({
-            type: actionTypes.SET_SEARCH_TERM,
-            term: input
-        })
-    }
+    dispatch({
+      type: actionTypes.SET_SEARCH_TERM,
+      term: input,
+    });
+  };
 
-    return (
-        <form className="search">
-            <div className="search__input">
-                <SearchIcon className="search__inputIcon" />
-                <input value={input} type='text'  to="/search" autocomplete="off" onChange={e => setInput(e.target.value)}  placeholder="Search Google or type a URL" />
-                <input onClick={search} ></input> 
-                <MicIcon />
-            </div>
-           <div ClassName="search__button">
-               <Button type="submit" onClick={search}> </Button>
-           </div>
-        </form>
-    )
+  return (
+    <form className="search">
+      <div className="search__input">
+        <SearchIcon className="search__inputIcon" />
+        <input
+          value={input}
+          type="text"
+          to="/search"
+          autocomplete="off"
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Search Google or type a URL"
+        />
+        <input onClick={search}></input>
+        <MicIcon />
+      </div>
+      <div ClassName="search__button">
+        <Button type="submit" onClick={search}>
+          {" "}
+        </Button>
+      </div>
+    </form>
+  );
 }
 
-export default Search
+export default Search;
--- google-clone/src/useGoogleSearch.js
+++ google-clone/src/useGoogleSearch.js
@@ -1,23 +1,24 @@
-import  {useState, useEffect} from 'react'
-import API_KEY from "./Keys"
+import { useState, useEffect } from "react";
+import API_KEY from "./Keys";
 
-const CONTEXT_KEY ="739d738a87f46ee45";
+const CONTEXT_KEY = "739d738a87f46ee45";
 
 const useGoogleSearch = (term) => {
-    const [data, setData] = useState(null);
-    
-    useEffect(() => {
-        const fetchData = async() => {
-            fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`)
-    
-    .then(response => response.json())
-    .then(result => {
-        setData(result)
-    })
- }
- fetchData();
-    }, [term])
-    return {data}
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          setData(result);
+        });
+    };
+    fetchData();
+  }, [term]);
+  return { data };
 };
 
-export default useGoogleSearch
+export default useGoogleSearch;
